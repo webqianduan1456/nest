b00c0d87f28b87d73b624d088d49d9d149f8ffb3
无间之水 <1989315788@qq.com>
2025-05-15 19:40:29 +0800

    首次提交：添加项目初始代码

--- src/User/User.module.ts
+++ src/User/User.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './User.controller';
 import { UserService } from './User.service';
+import { User } from 'src/Entity/User.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
