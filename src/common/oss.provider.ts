5603a05964974571f3de67179addcd13de65dce9
无间之水 <1989315788@qq.com>
2025-05-17 10:46:04 +0800

    首次提交：添加项目初始代码

--- src/common/oss.provider.ts
+++ src/common/oss.provider.ts
@@ -1,17 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Provider } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import OSS from 'ali-oss';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
+export const OSS_PROVIDER = 'OSS_CLIENT';
+interface OssConfig {
+  region: string;
+  accessKeyId: string;
+  accessKeySecret: string;
+  bucket: string;
+}
 export const OSSProvider: Provider = {
-  provide: 'OSS_CLIENT',
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService) =>
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    new OSS({
-      region: configService.get<string>('oss.region'), // 你的 OSS 地域
-      accessKeyId: configService.get<string>('oss.accessKeyId'), // 从环境变量读取
-      accessKeySecret: configService.get<string>('oss.accessKeySecret'), // 从环境变量读取
-      bucket: configService.get<string>('oss.bucket'), // 你的 Bucket 名称
-    }),
+  provide: OSS_PROVIDER,
+  useFactory: () => {
+    // 自动读取配置文件
+    const configPath = join(__dirname, '../../config/oss.json');
+    const config = JSON.parse(readFileSync(configPath, 'utf8')) as OssConfig;
+
+    // 简单验证
+    if (!config.accessKeyId || !config.accessKeySecret) {
+      throw new Error('阿里云OSS配置不完整！');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+    return new OSS(config);
+  },
 };
