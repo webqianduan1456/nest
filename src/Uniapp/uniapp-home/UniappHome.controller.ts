9b9e9f3a07e7df54afe4a2878dd975026945d763
无间之水 <1989315788@qq.com>
2025-10-07 21:13:26 +0800

    更换服务器重上传

--- src/Uniapp/uniapp-home/UniappHome.controller.ts
+++ src/Uniapp/uniapp-home/UniappHome.controller.ts
@@ -4,7 +4,7 @@ import { UniAppHomeService } from './UniappHome.service';
 @Controller('UniAppHome')
 export class UniAppHomeController {
   constructor(private readonly UniAppHomeServices: UniAppHomeService) {}
-
+  // 返回首页所有数据
   @Get('getData')
   getHomeData() {
     return this.UniAppHomeServices.getHomeData();
