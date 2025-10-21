761402685a184d2857531c3c7ccb2781cf361d6d
无间之水 <1989315788@qq.com>
2025-10-21 22:34:25 +0800

    提交所有接口

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { rateLimit } from 'express-rate-limit';
 import type { Express } from 'express';
 import * as compression from 'compression';
 
@@ -13,17 +12,6 @@ async function bootstrap() {
   instance.set('trust proxy', 1);
   // 启用 CORS（所有域名、所有方法都允许）
   app.enableCors();
-  // 限制请求次数
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000,
-      max: 100,
-      // 或者禁用 X-Forwarded-For 验证
-      skip: (req) => {
-        return !!req.headers['x-forwarded-for'];
-      },
-    }),
-  );
   // 减少传输体积压缩提高性能
   app.use(compression.default());
   await app.listen(3000, '0.0.0.0');
